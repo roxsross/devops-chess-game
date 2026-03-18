8ea6de8a5c40a8dfb42f66905e07f114753b39b3
roxsross <roxsrossve@gmail.com>
2026-03-18 19:21:06 -0300

    init

--- infrastructure/cdk.out/asset.ef44afe4a390f35004a944afda6a864ccad2b469465a26e302669f8b990311b1/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import gamesRouter from './games';
-import playersRouter from './players';
-
-const router = Router();
-router.use('/games', gamesRouter);
-router.use('/players', playersRouter);
-
-export default router;
